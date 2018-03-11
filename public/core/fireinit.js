67051d2ed55a049956c2238ee6d7756bc5e351c1
WilliamLQin <william.qin51@gmail.com>
2018-03-10 22:51:11 -0500

    install firebase

---
+++ public/core/fireinit.js
@@ -0,0 +1,11 @@
+// Initialize Firebase (development)
+var config = {
+  apiKey: "AIzaSyA13VqcsO2xQNrYuVNCtiYld6GJnPATwKM",
+  authDomain: "dev-first-powerup-compiler.firebaseapp.com",
+  databaseURL: "https://dev-first-powerup-compiler.firebaseio.com",
+  projectId: "dev-first-powerup-compiler",
+  storageBucket: "",
+  messagingSenderId: "480661376304"
+};
+firebase.initializeApp(config);
+
