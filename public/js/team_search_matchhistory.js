a5afec8f385b74a48b31fcebb5bac18af7099dbd
WilliamLQin <william.qin51@gmail.com>
2018-03-14 00:38:09 -0400

    modify data entry n/a to no attempt

---
+++ public/js/team_search_matchhistory.js
@@ -0,0 +1,70 @@
+function populateMatchHistory() {
+    matchHistoryAuto();
+    matchHistoryTeleop();
+    matchHistoryMisc();
+}
+
+function matchHistoryAuto() {
+
+    $('#auto_table').html("");
+
+    for (i = 0; i < data.match_number.length; i++) {
+
+        var row = $('<tr></tr>');
+
+        row.append($('<th scope="row"></th>').text(data.match_number[i]));
+        row.append($('<td></td>').text(data.auto_switch_score[i] + " (" + data.auto_switch_miss[i] + ")"));
+        row.append($('<td></td>').text(data.auto_scale_score[i] + " (" + data.auto_scale_miss[i] + ")"));
+        row.append($('<td></td>').text(data.auto_reachline[i] == 1 ? "Yes" : "No"));
+
+        $('#auto_table').append(row);
+
+    }
+
+}
+
+function matchHistoryTeleop() {
+
+    $('#teleop_table').html("");
+
+    for (i = 0; i < data.match_number.length; i++) {
+
+        var row = $('<tr></tr>');
+
+        row.append($('<th scope="row"></th>').text(data.match_number[i]));
+        row.append($('<td></td>').text(data.teleop_switch_score[i] + " (" + data.teleop_switch_miss[i] + ")"));
+        row.append($('<td></td>').text(data.teleop_scale_score[i] + " (" + data.teleop_scale_miss[i] + ")"));
+        row.append($('<td></td>').text(data.teleop_oppswitch_score[i] + " (" + data.teleop_oppswitch_miss[i] + ")"));
+        row.append($('<td></td>').text(data.teleop_vault[i]));
+
+        $('#teleop_table').append(row);
+
+    }
+
+}
+
+function matchHistoryMisc() {
+
+    $('#misc_table').html("");
+
+    for (i = 0; i < data.match_number.length; i++) {
+
+        var row = $('<tr></tr>');
+
+        row.append($('<th scope="row"></th>').text(data.match_number[i]));
+        row.append($('<td></td>').text(data.match_startpos[i][0].toUpperCase() + data.match_startpos[i].substring(1)));
+        row.append($('<td></td>').text(data.match_plates[i]));
+
+        var climb = "";
+        climb += data.climb_type[i][0].toUpperCase() + data.climb_type[i].substring(1);
+        climb += data.climb_lifted[i] ? ", Lifted" : "";
+        climb += data.climb_carried[i] ? ", Carried" : "";
+        climb += data.climb_parked[i] ? ", Parked" : "";
+
+        row.append($('<td></td>').text(climb));
+
+        $('#misc_table').append(row);
+
+    }
+
+}
