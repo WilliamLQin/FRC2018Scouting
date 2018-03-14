2829e15b1b32a205370e113d3f0fb995ef9e6f7c
WilliamLQin <william.qin51@gmail.com>
2018-03-14 10:32:29 -0400

    add prod compiler

--- public/core/fireinit.js
+++ public/core/fireinit.js
@@ -1,11 +1,21 @@
 // Initialize Firebase (development)
+// var config = {
+//   apiKey: "AIzaSyA13VqcsO2xQNrYuVNCtiYld6GJnPATwKM",
+//   authDomain: "dev-first-powerup-compiler.firebaseapp.com",
+//   databaseURL: "https://dev-first-powerup-compiler.firebaseio.com",
+//   projectId: "dev-first-powerup-compiler",
+//   storageBucket: "",
+//   messagingSenderId: "480661376304"
+// };
+// firebase.initializeApp(config);
+
+// Initialize Firebase
 var config = {
-  apiKey: "AIzaSyA13VqcsO2xQNrYuVNCtiYld6GJnPATwKM",
-  authDomain: "dev-first-powerup-compiler.firebaseapp.com",
-  databaseURL: "https://dev-first-powerup-compiler.firebaseio.com",
-  projectId: "dev-first-powerup-compiler",
-  storageBucket: "",
-  messagingSenderId: "480661376304"
+  apiKey: "AIzaSyAZnE6DXeZjsxvUi_qIQtd5tNz4y9K5_vY",
+  authDomain: "first-powerup-compiler.firebaseapp.com",
+  databaseURL: "https://first-powerup-compiler.firebaseio.com",
+  projectId: "first-powerup-compiler",
+  storageBucket: "first-powerup-compiler.appspot.com",
+  messagingSenderId: "295205244801"
 };
 firebase.initializeApp(config);
-
